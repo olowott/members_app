c106a80c5a9318f5b417f6ed948aaebf87fc0750
olowott <olowott@gmail.com>
2022-08-31 07:58:36 +0100

    all migrations created not executed to database yet

---
+++ database/migrations/2022_08_31_064325_add_columns_to_users.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //foregin tables
+            $table->foreignId('country_id')->nullable();
+            $table->foreignId('group_id')->nullable();
+            $table->foreignId('marital_status_id')->nullable();
+            $table->foreignId('hduhau_id')->nullable();
+            //other fields
+            $table->dropColumn('last_name')->nullable();
+            $table->string('address')->nullable();
+            $table->string('phone')->nullable()->unique();
+            $table->date('DOB')->nullable();
+            $table->date('membership_year')->nullable();
+            $table->enum('status', ['Active', 'Inactive', 'Blocked'])->default('Inactive');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->dropColumn('country_id');
+            $table->dropColumn('group_id');
+            $table->dropColumn('marital_status_id');
+            $table->dropColumn('hduhau_id');
+            $table->dropColumn('last_name');
+            $table->dropColumn('address');
+            $table->dropColumn('phone');
+            $table->dropColumn('DOB');
+            $table->dropColumn('membership_year');
+            $table->dropColumn('status', ['Active', 'Inactive', 'Blocked'])->default('Inactive');
+        });
+    }
+};
